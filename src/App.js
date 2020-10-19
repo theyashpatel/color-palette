8744b5b05c3f0bc00b1b438e2e8282eaafda966e
Yash <yashpatel26@gmail.com>
2020-10-19 14:51:54 -0400

    I don't know

--- src/App.js
+++ src/App.js
@@ -18,81 +18,82 @@ class App extends Component {
 
   resize = () => this.forceUpdate()
 
-componentDidMount() {
-  window.addEventListener('resize', this.resize)
-}
-
-componentWillUnmount() {
-  window.removeEventListener('resize', this.resize)
-}
-
-handleCopy(event) {
-  window.navigator.clipboard.writeText(event.target.title)
-}
-
-drawDivisions(noOfDivisions) {
-  const items = []
-  for (var i = 0; i < noOfDivisions; i++) {
-    var randomColor = getRandomColor()
-    items.push(<div onClick={this.handleCopy} key={i} title={randomColor} style={{backgroundColor: randomColor, position: "absolute", bottom: 0, top: "50%", width: window.innerWidth / noOfDivisions, marginLeft: (window.innerWidth/noOfDivisions) * i}}></div>)
+  componentDidMount() {
+    window.addEventListener('resize', this.resize)
   }
-  return (
-    <div>
-      {items}
-    </div>
-  )
-}
 
-handleTheme = (event, result) => {
-  this.setState({
-    theme: result.value
-  })
-}
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
 
-handleChange(event) {
-  const {name} = event.target
-  var value
-  if (name === "add") {
-    value = this.state.divisions + 1
+  handleCopy(event) {
+    window.navigator.clipboard.writeText(event.target.title)
   }
-  else if (name === "gen") {
-    value = this.state.divisions
+
+  drawDivisions(noOfDivisions) {
+    const items = []
+    for (var i = 0; i < noOfDivisions; i++) {
+      var randomColor = getRandomColor()
+      items.push(<div onClick={this.handleCopy} key={i} title={randomColor} style={{ backgroundColor: randomColor, position: "absolute", bottom: 0, top: "50%", width: window.innerWidth / noOfDivisions, marginLeft: (window.innerWidth / noOfDivisions) * i }}></div>)
+    }
+    return (
+      <div>
+        {items}
+      </div>
+    )
   }
-  else {
-    value = this.state.divisions - 1
+
+  handleTheme = (event, result) => {
+    this.setState({
+      theme: result.value
+    })
   }
-  if (value <= 0) {
-    value = 1
+
+  handleChange(event) {
+    const { name } = event.target
+    var value
+    if (name === "add") {
+      value = this.state.divisions + 1
+    }
+    else if (name === "gen") {
+      value = this.state.divisions
+    }
+    else {
+      value = this.state.divisions - 1
+    }
+    if (value <= 0) {
+      value = 1
+    }
+    this.setState({
+      divisions: value
+    })
   }
-  this.setState({
-    divisions: value
-  })
-}
 
   render() {
     const colorTheme = this.state.theme
+    // this is a change
     return (
       <div>
         <BrowserView>
           <CustomHeader themeColor={colorTheme} />
         </BrowserView>
-      <Segment.Group horizontal>
-      <CustomThemeSelect theme={colorTheme} handleTheme={this.handleTheme} />
-        <Segment textAlign="center">
-          <Header as='h1' color={colorTheme}>
-          {this.state.divisions}
-          </Header>
-        </Segment>
-        <Segment textAlign="center">
-          <Button.Group>
-            <Button basic color={colorTheme} name="add" onClick={this.handleChange}>+</Button>
-            <Button  basic color={colorTheme} name="sub" onClick={this.handleChange}>-</Button>
-          </Button.Group>
-        </Segment>
-        <Segment textAlign="center">
-        <Button basic color={colorTheme} name="gen" onClick={this.handleChange}>Generate</Button>
-        </Segment>
-      </Segment.Group>
+        <Segment.Group horizontal>
+          <CustomThemeSelect theme={colorTheme} handleTheme={this.handleTheme} />
+          <Segment textAlign="center">
+            <Header as='h1' color={colorTheme}>
+              {this.state.divisions}
+            </Header>
+          </Segment>
+          <Segment textAlign="center">
+            <Button.Group>
+              <Button basic color={colorTheme} name="add" onClick={this.handleChange}>+</Button>
+              <Button basic color={colorTheme} name="sub" onClick={this.handleChange}>-</Button>
+            </Button.Group>
+          </Segment>
+          <Segment textAlign="center">
+            <Button basic color={colorTheme} name="gen" onClick={this.handleChange}>Generate</Button>
+          </Segment>
+        </Segment.Group>
         {this.drawDivisions(this.state.divisions)}
       </div>
     )
