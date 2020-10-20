9716dcab9034b7ab8a175f63b8fd599462933005
Yash <yashpatel26@gmail.com>
2020-10-20 03:26:00 -0400

    fixed name the color bugs

--- src/data/NameTheColor.js
+++ src/data/NameTheColor.js
@@ -48,9 +48,9 @@ var ntc = {
       color = color.toUpperCase();
       if(color.length < 3 || color.length > 7)
         return ["#000000", "Invalid Color: " + color, false];
-      if(color.length % 3 == 0)
+      if(color.length % 3 === 0)
         color = "#" + color;
-      if(color.length == 4)
+      if(color.length === 4)
         color = "#" + color.substr(1, 1) + color.substr(1, 1) + color.substr(2, 1) + color.substr(2, 1) + color.substr(3, 1) + color.substr(3, 1);
   
       var rgb = ntc.rgb(color);
@@ -64,7 +64,7 @@ var ntc = {
   
       for(var i = 0; i < ntc.names.length; i++)
       {
-        if(color == "#" + ntc.names[i][0])
+        if(color === "#" + ntc.names[i][0])
           return ["#" + ntc.names[i][0], ntc.names[i][1], true];
   
         ndf1 = Math.pow(r - ntc.names[i][2], 2) + Math.pow(g - ntc.names[i][3], 2) + Math.pow(b - ntc.names[i][4], 2);
@@ -100,9 +100,9 @@ var ntc = {
       h = 0;
       if(delta > 0)
       {
-        if (max == r && max != g) h += (g - b) / delta;
-        if (max == g && max != b) h += (2 + (b - r) / delta);
-        if (max == b && max != r) h += (4 + (r - g) / delta);
+        if (max === r && max !== g) h += (g - b) / delta;
+        if (max === g && max !== b) h += (2 + (b - r) / delta);
+        if (max === b && max !== r) h += (4 + (r - g) / delta);
         h /= 6;
       }
       return [parseInt(h * 255), parseInt(s * 255), parseInt(l * 255)];
